4aa7449aec95db3a8ec9fe7a4b38a5be67460b39
GeorgeSchiro <9727github@schiro.name>
2022-07-23 15:28:32 -0400

    Improved support for non-persistent profiles.

--- ReplaceTextApp/Properties/AssemblyInfo.cs
+++ ReplaceTextApp/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1")]
-[assembly: AssemblyFileVersion("3.1")]
+[assembly: AssemblyVersion("3.2")]
+[assembly: AssemblyFileVersion("3.2")]
